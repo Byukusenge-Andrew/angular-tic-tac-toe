e7d70a27a581f9f7ebb3fd188a9835949787e23e
Byukusenge-Andrew <andrewbyukusenge4@gmail.com>
2025-04-07 10:16:44 +0200

    first commit

---
+++ src.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-src',
+  imports: [CommonModule],
+  templateUrl: './src.component.html',
+  styleUrl: './src.component.css'
+})
+export class SrcComponent {}
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-square',
+  imports: [CommonModule],
+  templateUrl: './square.component.html',
+  styleUrl: './square.component.scss'
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O' | undefined;
+}
